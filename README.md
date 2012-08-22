fe2225de49c90c38270e3d27e38d3e3a657a4454
Wikinaut <mail@tgries.de>
2012-08-22 20:32:28 +0200

    initial import of my echo-server test suite

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 echo-server--node-
 ==================
 
-Simple test program for node.js, express and socket.io which echoes characters entered in the client (browser) via a server
+Simple test program for node.js, express and socket.io which echoes characters entered in the client (browser) via a server.
+
+Requirements:
+
+* node ( wget node; ./configure; make; make install )
+* git clone echo-server
+* socket.io ( npm install socket.io )
+* express (npm install express )
---
+++ echo-server.js
@@ -0,0 +1,56 @@
+/**
+* Socket.IO server (single process only)
+*/
+var express = require("express");
+
+// see https://github.com/visionmedia/express/wiki/Migrating-from-2.x-to-3.x
+// https://gist.github.com/1036976 very simple socket.io@0.7 echo server 
+
+var app = express();
+var http = require('http');
+var server = http.createServer(app);
+// var io = require('socket.io').listen(server);
+var socketio = require('socket.io');
+var io = socketio.listen(server);
+
+server.listen(9002);
+
+io.set('transports', [
+    'websocket'
+  , 'flashsocket'
+  , 'htmlfile'
+  , 'xhr-polling'
+  , 'jsonp-polling'
+]);
+
+// https://github.com/LearnBoost/socket.io/issues/283 
+// lower the number to lessen the debug output
+// log level 0 or 1 : info output
+// 2: debug output
+io.set( 'log level', 2 );
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/client.html');
+});
+
+io.sockets.on('connection', function (socket) {
+  console.log('connection established');
+  io.sockets.emit('announcement', "connected" );
+
+  socket.on('user message', function (msg) {
+    // console.log( "received: " + msg);
+
+    // socket.broadcast.emit('user message', msg);
+    if ( msg == 27 ) {
+	io.sockets.emit('announcement', "Socket stopped at server side. You can reestablish the socket connection by restarting your server." );
+
+	// for server-side disconnect see https://github.com/LearnBoost/socket.io/issues/795
+	socket.disconnect();
+	
+    } else {
+        io.sockets.emit('user message', msg);
+	io.sockets.emit('announcement', "connected" );
+    }
+  });
+
+});
